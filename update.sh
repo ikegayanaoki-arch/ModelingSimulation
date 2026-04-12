61773d44baa46b07ee27195c38c905de6125a4ab
Naoki Ikegaya <ikegaya@SpanishMackerel.local>
2026-04-12 15:01:46 +0900

    20260410-01

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@
  codex exec \
     -C /Users/ikegaya/programs/LLMWorkSpace/git/lecMaterial.dev/MaS/site \
     --sandbox workspace-write \
-    --ask-for-approval never \
+    -- --ask-for-approval never \
     "このディレクトリ以下の"$DATE"の中のhtmlファイルへのリンクをindex.htmlに追加．日本語と英語それぞれに対応するリンクと目次を入れる"
 
  git add .

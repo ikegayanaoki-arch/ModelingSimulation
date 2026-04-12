060b115ec4e739a1a053a6fa7cb3f016390799e4
Naoki Ikegaya <ikegaya@SpanishMackerel.local>
2026-04-12 15:04:09 +0900

    20260410-01

--- update.sh
+++ update.sh
@@ -3,12 +3,11 @@
 
  DATE=20260410-01
  cp -r ../lec/$DATE/html ./$DATE
- 
+
  codex exec \
     -C /Users/ikegaya/programs/LLMWorkSpace/git/lecMaterial.dev/MaS/site \
     --sandbox workspace-write \
-    -- --ask-for-approval never \
-    "このディレクトリ以下の"$DATE"の中のhtmlファイルへのリンクをindex.htmlに追加．日本語と英語それぞれに対応するリンクと目次を入れる"
+    "このディレクトリ以下の ${DATE} の中の html ファイルへのリンクを index.html に追加。日本語と英語それぞれに対応するリンクと目次を入れる。"
 
  git add .
  git commit -m $DATE

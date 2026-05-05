e219422a545db6668686be87ad170482dc2b12be
Naoki Ikegaya <ikegaya@SpanishMackerel.local>
2026-05-05 16:34:26 +0900

    20260424-03

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@
  codex exec --skip-git-repo-check \
     -C /Users/ikegaya/programs/LLMWorkSpace/git/lecMaterial.dev/MaS/site \
     --sandbox workspace-write \
-    "このディレクトリ以下の ${DATE} の中の html/index_en.html へのリンクを index.html のLecture Recordsに追加．日本語も同様に更新"
+    "このディレクトリ以下の ${DATE} の中の index_en.html へのリンクを index.html のLecture Recordsに追加．日本語も同様に更新"
 
  git add .
  git commit -m $DATE
